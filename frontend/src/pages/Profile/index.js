3ffc35e98222713383c80257db2566332b2f52b9
ano.emangi <ano.emangi@minutoseguros.com.br>
2020-08-16 16:05:02 -0300

    Foi feito a tela principal

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,29 +1,47 @@
-import React from 'react';
-// import {Link} from 'react-router-dom';
-import {BsLock} from 'react-icons/bs';
+import React, {useState, useEffect} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {FiPower} from 'react-icons/fi';
 
 import './styles.css';
+import api from '../../services/api';
+
 
 
 export default function Profile(){
+const [cursos, setCurso] = useState([]);
 
+const history = useHistory()
+useEffect(() =>{
+    api.get('inicio').then(Response =>{
+        setCurso(Response.data);
+    })
+});
+
+function handLogout(){
+    localStorage.clear();
+    history.push('/');
+}
 
 return(
-    <div className="profile-container">
-        <header className="header-container">
-          <h1>teste</h1>
-          <BsLock size={20} />
+<div className="profile-container">
+        <header>
+            <span>Bem vinda</span>
+            <button onClick={handLogout} type="button">
+              <FiPower size={18} color= "#E02041"/>
+            </button>
         </header>
 
-
-        <h1>Hello Xuxa ☺</h1>
-
-    
-        <footer className="footer-container">
-        <span>
-          Desenvolvido com favorite  por <strong>Arnoboy<span className="red">S</span></strong>
-        </span>
-        </footer>
+        <h1>Curso da Lingua Francês</h1>
+        <ul>
+            {cursos.map(curso =>(
+                <li key={curso.id}>
+                    <strong>Curso Sobre:</strong>
+                    <Link className="back-link" to="/">
+                    {curso.tipoCurso}
+                    </Link>
+                </li>
+            ))}
+        </ul>
     </div>
 );
 }
